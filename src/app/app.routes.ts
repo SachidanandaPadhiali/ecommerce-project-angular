f353eeb393208208f59ddfb1a71dd795e4ce3005
Sachidananda Padhiali <sagarpadhiali99@gmail.com>
2025-07-18 21:18:29 +0530

    Adding Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { SellerAuth } from './seller-auth/seller-auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: Home,
+        path: ''
+    },
+    {
+        component: SellerAuth,
+        path: 'seller-auth'
+    },
+];
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
