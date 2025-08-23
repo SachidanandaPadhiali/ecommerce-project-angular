d7ecdd1998d697b9db06562313f8917a6c1a6e67
Sachidananda Padhiali <sagarpadhiali99@gmail.com>
2025-08-23 23:13:53 +0530

    feat(checkout): Made Review selection module in frontend responsive
    - Added UI components for viewing the items, address, payment and shipping and place order
    - Improved UX and frontend to show the price in every section properly

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.222.153.nip.io:8088/api'
+  apiUrl: 'http://192.168.120.153.nip.io:8088/api'
 };
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.222.153.nip.io:8088/api'
+  apiUrl: 'http://192.168.120.153.nip.io:8088/api'
 };
