368d2d0ee144717ee120348516e70827e3b977bc
Sachidananda Padhiali <sagarpadhiali99@gmail.com>
2025-09-16 21:00:11 +0530

    feat(product-detail): Formatting for product detail component UI with demo data to check for product availability in wishlist and cart

--- front-end/src/app/product-detail/product-detail.ts
+++ front-end/src/app/product-detail/product-detail.ts
@@ -9,42 +9,65 @@ import { ProductCard } from '../product-card/product-card';
 import { Cart } from '../models/Cart.model';
 import { forkJoin, Observable } from 'rxjs';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart as fasHeart, faCartShopping, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'; // regular (outline)
+import { Shop } from '../shop/shop';
+
 @Component({
   selector: 'app-product-detail',
-  imports: [CommonModule, ProductCard, RouterModule],
+  imports: [CommonModule, ProductCard, RouterModule, FontAwesomeModule, Shop],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css'
 })
 export class ProductDetail implements OnInit {
-  productId: number = 0;
-  product$!: Observable<Product>;
-  category: string = '';
+
+  prodWish = farHeart;
+  prodWished = fasHeart;
+  cart = faCartShopping;
+  delete = faTrash;
+
   loading: boolean = true;
-  wished: boolean = false;
-  wishList: Set<number> = new Set();
+  error = '';
+  product$!: Observable<Product>;
+
   userId: number = 0;
   prodId: number = 0;
-  error = '';
-  curUserCart: Cart | null = null;
-  cartMap = new Map<number, number>();
-  cartProductIds: Set<number> = new Set();
+  isWished: boolean = true;
+  isInCart: boolean = false;
+  cartCount: number = 2;
+
+  maxStars = [1, 2, 3, 4, 5];
+
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
     private userService: UserService,
     private router: Router,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private shop: Shop
   ) { }
 
   ngOnInit(): void {
     this.product$ = this.route.paramMap.pipe(
       switchMap(params => {
-        const id = params.get('productId');
-        return this.productService.getProductById(id ? +id : 0);
+    console.log('Product ID:', params);
+        this.prodId = Number(params.get('productId')) || 0;
+        return this.productService.getProductById(this.prodId ? +this.prodId : 0);
       })
     );
 
     this.userId = JSON.parse(localStorage.getItem('user') || '{}')?.id;
+  }
+  getFill(n: number, rating: number | undefined): string {
+    const safeRating = rating ?? 0;  // fallback to 0 if undefined
+    // your logic here
+    return (Math.min(safeRating - n + 1, 1) * 100) + '%';
+  }
+
+  toggleWish(){
+    this.shop.toggleWish(this.prodId);
+    this.cdr.detectChanges();
   }
 }
